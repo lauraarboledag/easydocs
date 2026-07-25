c5e576279bf34821926a83b9eb690f6cd4599942
lauraarboledag <laura.arboledag@est.iudigital.edu.co>
2026-07-25 12:10:12 -0500

    configuracion de main para conectar front desplegado

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL],
+    allow_origins=[
+        settings.FRONTEND_URL,
+        "https://easydocs-kappa.vercel.app",
+        "https://*.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
